d4c5da0ed3f7a3708215860c5a0c0eaa10b9ceb0
Ricardo Lecheta <rlecheta@livetouch.com.br>
2018-03-03 15:53:30 -0300

    ok

--- Carros/app/src/main/java/br/com/livroandroid/carros/activity/BaseActivity.java
+++ Carros/app/src/main/java/br/com/livroandroid/carros/activity/BaseActivity.java
@@ -1,4 +1,15 @@
 package br.com.livroandroid.carros.activity;
+
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import br.com.livroandroid.carros.R;
+
 public class BaseActivity extends AppCompatActivity {
+    protected void setUpToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+        }
+    }
 }
